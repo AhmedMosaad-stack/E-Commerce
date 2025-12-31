4b4260b8df0a222b6205010623337a09cdffef30
Ahmed Mosaad <ahmedmosad840@gmail.com>
2025-12-31 04:06:06 +0200

    Details button no-login edit

--- app/_components/AddCartButton.tsx
+++ app/_components/AddCartButton.tsx
@@ -4,11 +4,24 @@ import { CartContext } from "@/context/CartContext";
 import { addToCart } from "@/lib/services/addToCart";
 import React, { useState, useContext } from "react";
 import { toast } from "sonner";
+import { useSession } from "next-auth/react";
 
 export default function AddCartButton({ id, className, loadingClassName }: { id: string; className?: string; loadingClassName?: string }) {
   const { cartNumber, setcartNumber } = useContext(CartContext)!;
   const [isLoading, setisLoading] = useState(false);
+  const { data: session } = useSession();
+  
   async function checkAddtoCart(id: string) {
+    // Check if user is logged in first
+    if (!session) {
+      toast.error("Please Login to add to Cart!", {
+        position: "top-center",
+        duration: 3000,
+        style: { backgroundColor: "#ffffff", color: "red" },
+      });
+      return;
+    }
+    
     try {
       setisLoading(true);
       const res = await addToCart(id);
