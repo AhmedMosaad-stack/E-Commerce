eb258cd7d9cbfe5cbf98f964f9fe6abc661e8b5c
Ahmed Mosaad <ahmedmosad840@gmail.com>
2026-01-06 19:06:09 +0200

    Last home section responsive

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,10 @@ import { getAllcategories } from "@/lib/services/getAllcategories";
 import Image from "next/image";
 import jbl from "../public/JBL.png"
 import { Button } from "@/components/ui/button";
-import Services from "./_components/Services";
 import ScrollAnimation from "./_components/ScrollAnimation";
+import { TbTruckDelivery } from "react-icons/tb";
+import { RiCustomerService2Line } from "react-icons/ri";
+import { MdOutlineVerifiedUser } from "react-icons/md";
 export default async function Home() {
   const categories = await getAllcategories();
   const products = await getAllProducts();
@@ -39,8 +41,43 @@ export default async function Home() {
       <div className="scroll-section">
         <AllProducts products={products} />
       </div>
-      <div className="scroll-section">
-        <Services/>
+      <div className="scroll-section w-[90%] mx-auto mb-20 grid grid-cols-1 md:grid-cols-3 gap-12 md:gap-20">
+        {/* Free Delivery */}
+        <div className="flex flex-col items-center text-center">
+          <div className="w-20 h-20 rounded-full bg-gray-300 flex items-center justify-center mb-6">
+            <div className="w-14 h-14 rounded-full bg-black flex items-center justify-center">
+              <TbTruckDelivery className="text-3xl text-white" />
+            </div>
+          </div>
+          <h3 className="text-xl font-semibold mb-2">
+            FREE AND FAST DELIVERY
+          </h3>
+          <p className="text-sm">Free delivery for all orders over $140</p>
+        </div>
+
+        {/* Customer Service */}
+        <div className="flex flex-col items-center text-center">
+          <div className="w-20 h-20 rounded-full bg-gray-300 flex items-center justify-center mb-6">
+            <div className="w-14 h-14 rounded-full bg-black flex items-center justify-center">
+              <RiCustomerService2Line className="text-3xl text-white" />
+            </div>
+          </div>
+          <h3 className="text-xl font-semibold mb-2">
+            24/7 CUSTOMER SERVICE
+          </h3>
+          <p className="text-sm">Friendly 24/7 customer support</p>
+        </div>
+
+        {/* Money Back */}
+        <div className="flex flex-col items-center text-center">
+          <div className="w-20 h-20 rounded-full bg-gray-300 flex items-center justify-center mb-6">
+            <div className="w-14 h-14 rounded-full bg-black flex items-center justify-center">
+              <MdOutlineVerifiedUser className="text-3xl text-white" />
+            </div>
+          </div>
+          <h3 className="text-xl font-semibold mb-2">MONEY BACK GUARANTEE</h3>
+          <p className="text-sm">We reurn money within 30 days</p>
+        </div>
       </div>
     </>
   );
